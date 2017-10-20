c4d362cc88bdf6efb51e9d8aaf75578a0aee9047
Emanuel Posescu <emanuel.posescu@bursatransport.com>
2017-10-20 15:04:16 +0300

    Code formating.

--- esp8266-xpressnet-z21.ino
+++ esp8266-xpressnet-z21.ino
@@ -13,9 +13,9 @@
 #include <ESP8266WiFi.h>
 
 // WiFiManager auto-configuration library
-#include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
-#include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
-#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
+#include <DNSServer.h>        //Local DNS Server used for redirecting all requests to the configuration portal
+#include <ESP8266WebServer.h> //Local WebServer used to serve the configuration portal
+#include <WiFiManager.h>      //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
 
 // WiFi UDP library
 #include <WiFiUdp.h>
@@ -26,29 +26,29 @@
 #define XNetRS485_RX 2    // RS485 RX pin
 #define RS485_TXRX_PIN 12 // RS485 TX control pin (can be ommited for auto TX control)
 // XpressNet settings
-byte XNetAddress = 30;    // The XpressNet address of this device
+byte XNetAddress = 30; // The XpressNet address of this device
 #include <esp8266-XpressNet.h>
 
 // Z21 library and settings
-#define Z21_UDP_TX_MAX_SIZE 64  // max received UDP packet size
-#define z21Port 21105     // z21 UDP port to listen on
+#define Z21_UDP_TX_MAX_SIZE 64 // max received UDP packet size
+#define z21Port 21105          // z21 UDP port to listen on
 #include <z21.h>
 
 // S88 settings
-#define S88DataPin 15       //S88 Data IN pin
-#define S88ClkPin 13        //S88 Clock pin
-#define S88PSPin 12         //S88 PS/LOAD pin
-#define S88ResetPin 14      //S88 Reset pin
-byte S88Module = 2;         // Number of S88 modules. Each module has 8 inputs so a 16 inputs board is composed of 2 modules
+#define S88DataPin 15  //S88 Data IN pin
+#define S88ClkPin 13   //S88 Clock pin
+#define S88PSPin 12    //S88 PS/LOAD pin
+#define S88ResetPin 14 //S88 Reset pin
+byte S88Module = 2;    // Number of S88 modules. Each module has 8 inputs so a 16 inputs board is composed of 2 modules
 
 extern "C" {
-  #include "user_interface.h"
+#include "user_interface.h"
 }
 os_timer_t s88Timer;
-uint8_t S88RCount = 0;    //Lesezähler 0-39 Zyklen
-uint8_t S88RMCount = 0;   //Lesezähler Modul-Pin
-char S88sendon = '0';        //Bit Änderung
-byte data[62];     //Zustandsspeicher für 62x 8fach Modul
+uint8_t S88RCount = 0;  //Lesezähler 0-39 Zyklen
+uint8_t S88RMCount = 0; //Lesezähler Modul-Pin
+char S88sendon = '0';   //Bit Änderung
+byte data[62];          //Zustandsspeicher für 62x 8fach Modul
 
 // Debug variants:
 // 1. Via hardware serial
@@ -58,8 +58,8 @@ byte data[62];     //Zustandsspeicher für 62x 8fach Modul
 //#define DEBUGSERIAL SoftwareSerial(1,2);
 
 // IP settings
-#define maxIP 20      //Total number of storred IP address (clients)
-typedef struct        // Structure to hold IP's and ports
+#define maxIP 20 //Total number of storred IP address (clients)
+typedef struct   // Structure to hold IP's and ports
 {
   byte IP0;
   byte IP1;
@@ -67,8 +67,8 @@ typedef struct        // Structure to hold IP's and ports
   byte IP3;
   uint16_t port;
 } listofIP;
-listofIP mem[maxIP];  // IP storage
-byte storedIP = 0;    // number of currently stored IPs
+listofIP mem[maxIP]; // IP storage
+byte storedIP = 0;   // number of currently stored IPs
 
 // Init local variables
 unsigned char packetBuffer[Z21_UDP_TX_MAX_SIZE];
@@ -76,10 +76,9 @@ WiFiUDP Udp;
 z21Class z21;
 XpressNetClass XpressNet;
 
-
-
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
+// put your setup code here, to run once:
 #if defined(DEBUGSERIAL)
   DEBUGSERIAL.begin(115200);
   DEBUGSERIAL.println();
@@ -92,11 +91,12 @@ void setup() {
   wifiManager.autoConnect("Z21-Config");
 
 #if defined(DEBUGSERIAL)
-    DEBUGSERIAL.println(F("WiFi connected"));
-    DEBUGSERIAL.print(F("Local IP: ")); DEBUGSERIAL.println(WiFi.localIP());
+  DEBUGSERIAL.println(F("WiFi connected"));
+  DEBUGSERIAL.print(F("Local IP: "));
+  DEBUGSERIAL.println(WiFi.localIP());
 #endif
 
-  // Start XpressNet
+// Start XpressNet
 #if defined(RS485_TXRX_PIN)
   XpressNet.start(XNetAddress, RS485_TXRX_PIN);
 #else
@@ -111,23 +111,26 @@ void setup() {
   z21.setPower(csNormal);
 }
 
-void loop() {
+void loop()
+{
   // Receive XpressNet packets
   XpressNet.receive();
-  
+
   // Receive UDP packets and send them to z21 library
-  if (Udp.parsePacket() > 0) { //packetSize
+  if (Udp.parsePacket() > 0)
+  {                                              //packetSize
     Udp.read(packetBuffer, Z21_UDP_TX_MAX_SIZE); // read the packet into packetBufffer
     IPAddress remote = Udp.remoteIP();
     z21.receive(addIP(remote[0], remote[1], remote[2], remote[3], Udp.remotePort()), packetBuffer);
   }
-
 }
 
 // Store IP in list and return it's index
-byte addIP (byte ip0, byte ip1, byte ip2, byte ip3, uint16_t port) {
+byte addIP(byte ip0, byte ip1, byte ip2, byte ip3, uint16_t port)
+{
   //suche ob IP schon vorhanden?
-  for (byte i = 0; i < storedIP; i++) {
+  for (byte i = 0; i < storedIP; i++)
+  {
     if (mem[i].IP0 == ip0 && mem[i].IP1 == ip1 && mem[i].IP2 == ip2 && mem[i].IP3 == ip3)
       return i + 1;
   }
@@ -141,8 +144,6 @@ byte addIP (byte ip0, byte ip1, byte ip2, byte ip3, uint16_t port) {
   storedIP++;
   return storedIP;
 }
-
-
 
 //--------------------------------------------------------------------------------------------
 // z21 library callback functions
@@ -158,17 +159,20 @@ void notifyz21RailPower(uint8_t State)
 //--------------------------------------------------------------------------------------------
 void notifyz21EthSend(uint8_t client, uint8_t *data)
 {
-  if (client == 0) { //all stored
-    for (byte i = 0; i < storedIP; i++) {
+  if (client == 0)
+  { //all stored
+    for (byte i = 0; i < storedIP; i++)
+    {
       IPAddress ip(mem[i].IP0, mem[i].IP1, mem[i].IP2, mem[i].IP3);
-      Udp.beginPacket(ip, mem[i].port);    //Broadcast
+      Udp.beginPacket(ip, mem[i].port); //Broadcast
       Udp.write(data, data[0]);
       Udp.endPacket();
     }
   }
-  else {
+  else
+  {
     IPAddress ip(mem[client - 1].IP0, mem[client - 1].IP1, mem[client - 1].IP2, mem[client - 1].IP3);
-    Udp.beginPacket(ip, mem[client - 1].port);    //no Broadcast
+    Udp.beginPacket(ip, mem[client - 1].port); //no Broadcast
     Udp.write(data, data[0]);
     Udp.endPacket();
   }
@@ -177,7 +181,7 @@ void notifyz21EthSend(uint8_t client, uint8_t *data)
 //--------------------------------------------------------------------------------------------
 void notifyz21S88Data(uint8_t gIndex)
 {
-  //z21.setS88Data (datasend);  //Send back state of S88 Feedback
+//z21.setS88Data (datasend);  //Send back state of S88 Feedback
 #if defined(DEBUGSERIAL)
   DEBUGSERIAL.printf("notifyz21S88Data %d\r\n", gIndex);
 #endif
@@ -205,7 +209,7 @@ void notifyz21LocoSpeed(uint16_t Adr, uint8_t speed, uint8_t steps)
 #if defined(DEBUGSERIAL)
   DEBUGSERIAL.printf("notifyz21LocoSpeed %d %d %d\r\n", Adr, speed, steps);
 #endif
-XpressNet.setLocoDrive((Adr >> 8) & 0x3F, (Adr & 0xFF), steps, speed);
+  XpressNet.setLocoDrive((Adr >> 8) & 0x3F, (Adr & 0xFF), steps, speed);
 }
 
 //--------------------------------------------------------------------------------------------
@@ -268,23 +272,18 @@ void notifyz21CVPOMWRITEBYTE(uint16_t Adr, uint16_t cvAdr, uint8_t value)
 #endif
 }
 
-
-
-
-
-
 //--------------------------------------------------------------------------------------------
 // xpressnet library callback functions
 //--------------------------------------------------------------------------------------------
-void notifyXNetStatus(uint8_t LedState )
+void notifyXNetStatus(uint8_t LedState)
 {
-//#if defined(DEBUGSERIAL)
-//  DEBUGSERIAL.printf("notifyXNetStatus %d\r\n", LedState);
-//#endif
+  //#if defined(DEBUGSERIAL)
+  //  DEBUGSERIAL.printf("notifyXNetStatus %d\r\n", LedState);
+  //#endif
 }
 
 //--------------------------------------------------------------------------------------------
-void notifyXNetVer(uint8_t V, uint8_t ID )
+void notifyXNetVer(uint8_t V, uint8_t ID)
 {
 #if defined(DEBUGSERIAL)
   DEBUGSERIAL.printf("notifyXNetVer %d %d\r\n", V, ID);
@@ -292,7 +291,7 @@ void notifyXNetVer(uint8_t V, uint8_t ID )
 }
 
 //--------------------------------------------------------------------------------------------
-void notifyXNetPower(uint8_t State )
+void notifyXNetPower(uint8_t State)
 {
 #if defined(DEBUGSERIAL)
   DEBUGSERIAL.printf("notifyXNetPower %d\r\n", State);
@@ -300,7 +299,7 @@ void notifyXNetPower(uint8_t State )
 }
 
 //--------------------------------------------------------------------------------------------
-void notifyLokFunc(uint8_t Adr_High, uint8_t Adr_Low,  uint8_t F2, uint8_t F3 )
+void notifyLokFunc(uint8_t Adr_High, uint8_t Adr_Low, uint8_t F2, uint8_t F3)
 {
 #if defined(DEBUGSERIAL)
   DEBUGSERIAL.printf("notifyLokFunc %d %d %d %d\r\n", Adr_High, Adr_Low, F2, F3);
@@ -308,7 +307,7 @@ void notifyLokFunc(uint8_t Adr_High, uint8_t Adr_Low,  uint8_t F2, uint8_t F3 )
 }
 
 //--------------------------------------------------------------------------------------------
-void notifyLokAll(uint8_t Adr_High, uint8_t Adr_Low, boolean Busy, uint8_t Steps, uint8_t Speed, uint8_t Direction, uint8_t F0, uint8_t F1, uint8_t F2, uint8_t F3, boolean Req )
+void notifyLokAll(uint8_t Adr_High, uint8_t Adr_Low, boolean Busy, uint8_t Steps, uint8_t Speed, uint8_t Direction, uint8_t F0, uint8_t F1, uint8_t F2, uint8_t F3, boolean Req)
 {
 #if defined(DEBUGSERIAL)
   DEBUGSERIAL.printf("notifyLokAll %d %d %d %d %d %d %d %d %d\r\n", Adr_High, Adr_Low, Steps, Speed, Direction, F0, F1, F2, F3);
@@ -316,7 +315,7 @@ void notifyLokAll(uint8_t Adr_High, uint8_t Adr_Low, boolean Busy, uint8_t Steps
 }
 
 //--------------------------------------------------------------------------------------------
-void notifyCVInfo(uint8_t State )
+void notifyCVInfo(uint8_t State)
 {
 #if defined(DEBUGSERIAL)
   DEBUGSERIAL.printf("notifyCVInfo %d\r\n", State);
@@ -324,7 +323,7 @@ void notifyCVInfo(uint8_t State )
 }
 
 //--------------------------------------------------------------------------------------------
-void notifyCVResult(uint8_t cvAdr, uint8_t cvData )
+void notifyCVResult(uint8_t cvAdr, uint8_t cvData)
 {
 #if defined(DEBUGSERIAL)
   DEBUGSERIAL.printf("notifyCVResult %d %d\r\n", cvAdr, cvData);
@@ -339,97 +338,106 @@ void notifyTrnt(uint8_t Adr_High, uint8_t Adr_Low, uint8_t Pos)
 #endif
 }
 
-
-
-
-
-
 //--------------------------------------------------------------------------------------------
 // S88 functions
 //--------------------------------------------------------------------------------------------
-void SetupS88() {
-   if (S88Module > 62 || S88Module == 0) { //S88 off!
-     S88Module = 0;
-     return;
-   }
-  
-   os_timer_setfn(&s88Timer, S88Timer, NULL);
-   os_timer_arm(&s88Timer, 1, true);
+void SetupS88()
+{
+  if (S88Module > 62 || S88Module == 0)
+  { //S88 off!
+    S88Module = 0;
+    return;
   }
-  
-  //-------------------------------------------------------------- 
-  //S88 Timer ISR Routine
-  void S88Timer(void *pArg) {
-   if (S88RCount == 3)    //Load/PS Leitung auf 1, darauf folgt ein Schiebetakt nach 10 ticks!
-     digitalWrite(S88PSPin, HIGH);
-   if (S88RCount == 4)   //Schiebetakt nach 5 ticks und S88Module > 0
-     digitalWrite(S88ClkPin, HIGH);       //1. Impuls
-   if (S88RCount == 5)   //Read Data IN 1. Bit und S88Module > 0
-     S88readData();    //LOW-Flanke während Load/PS Schiebetakt, dann liegen die Daten an
-   if (S88RCount == 9)    //Reset-Plus, löscht die den Paralleleingängen vorgeschaltetetn Latches
-     digitalWrite(S88ResetPin, HIGH);
-   if (S88RCount == 10)    //Ende Resetimpuls
-     digitalWrite(S88ResetPin, LOW);
-   if (S88RCount == 11)    //Ende PS Phase
-     digitalWrite(S88PSPin, LOW);
-   if (S88RCount >= 12 && S88RCount < 10 + (S88Module * 8) * 2) {    //Auslesen mit weiteren Schiebetakt der Latches links
-     if (S88RCount % 2 == 0)      //wechselnder Taktimpuls/Schiebetakt
-       digitalWrite(S88ClkPin, HIGH);  
-     else S88readData();    //Read Data IN 2. bis (Module*8) Bit
-   }
-   S88RCount++;      //Zähler für Durchläufe/Takt
-   if (S88RCount >= 10 + (S88Module * 8) * 2) {  //Alle Module ausgelesen?
-     S88RCount = 0;                    //setzte Zähler zurück
-     S88RMCount = 0;                  //beginne beim ersten Modul von neuem
-     //init der Grundpegel
-     digitalWrite(S88PSPin, LOW);    
-     digitalWrite(S88ClkPin, LOW);
-     digitalWrite(S88ResetPin, LOW);
-     if (S88sendon == 's')  //Änderung erkannt
-       S88sendon = 'i';      //senden
-   }
-   //Capture the current timer value. This is how much error we have due to interrupt latency and the work in this function
+
+  os_timer_setfn(&s88Timer, S88Timer, NULL);
+  os_timer_arm(&s88Timer, 1, true);
+}
+
+//--------------------------------------------------------------
+//S88 Timer ISR Routine
+void S88Timer(void *pArg)
+{
+  if (S88RCount == 3) //Load/PS Leitung auf 1, darauf folgt ein Schiebetakt nach 10 ticks!
+    digitalWrite(S88PSPin, HIGH);
+  if (S88RCount == 4)              //Schiebetakt nach 5 ticks und S88Module > 0
+    digitalWrite(S88ClkPin, HIGH); //1. Impuls
+  if (S88RCount == 5)              //Read Data IN 1. Bit und S88Module > 0
+    S88readData();                 //LOW-Flanke während Load/PS Schiebetakt, dann liegen die Daten an
+  if (S88RCount == 9)              //Reset-Plus, löscht die den Paralleleingängen vorgeschaltetetn Latches
+    digitalWrite(S88ResetPin, HIGH);
+  if (S88RCount == 10) //Ende Resetimpuls
+    digitalWrite(S88ResetPin, LOW);
+  if (S88RCount == 11) //Ende PS Phase
+    digitalWrite(S88PSPin, LOW);
+  if (S88RCount >= 12 && S88RCount < 10 + (S88Module * 8) * 2)
+  {                         //Auslesen mit weiteren Schiebetakt der Latches links
+    if (S88RCount % 2 == 0) //wechselnder Taktimpuls/Schiebetakt
+      digitalWrite(S88ClkPin, HIGH);
+    else
+      S88readData(); //Read Data IN 2. bis (Module*8) Bit
+  }
+  S88RCount++; //Zähler für Durchläufe/Takt
+  if (S88RCount >= 10 + (S88Module * 8) * 2)
+  {                 //Alle Module ausgelesen?
+    S88RCount = 0;  //setzte Zähler zurück
+    S88RMCount = 0; //beginne beim ersten Modul von neuem
+    //init der Grundpegel
+    digitalWrite(S88PSPin, LOW);
+    digitalWrite(S88ClkPin, LOW);
+    digitalWrite(S88ResetPin, LOW);
+    if (S88sendon == 's') //Änderung erkannt
+      S88sendon = 'i';    //senden
+  }
+  //Capture the current timer value. This is how much error we have due to interrupt latency and the work in this function
   //  TCNT2 = TCNT2 + TIMER_Time;    //Reload the timer and correct for latency.
+}
+
+// //--------------------------------------------------------------
+// //Einlesen des Daten-Bit und Vergleich mit vorherigem Durchlauf
+void S88readData()
+{
+  digitalWrite(S88ClkPin, LOW); //LOW-Flanke, dann liegen die Daten an
+  byte Modul = S88RMCount / 8;
+  byte Port = S88RMCount % 8;
+  byte getData = digitalRead(S88DataPin); //Bit einlesen
+  if (bitRead(data[Modul], Port) != getData)
+  {                                       //Zustandsänderung Prüfen?
+    bitWrite(data[Modul], Port, getData); //Bitzustand Speichern
+    S88sendon = 's';                      //Änderung vorgenommen. (SET)
   }
-  
-  // //--------------------------------------------------------------
-  // //Einlesen des Daten-Bit und Vergleich mit vorherigem Durchlauf
-  void S88readData() {
-   digitalWrite(S88ClkPin, LOW);  //LOW-Flanke, dann liegen die Daten an
-   byte Modul = S88RMCount / 8;
-   byte Port = S88RMCount % 8;
-   byte getData = digitalRead(S88DataPin);  //Bit einlesen
-   if (bitRead(data[Modul],Port) != getData) {     //Zustandsänderung Prüfen?
-     bitWrite(data[Modul],Port,getData);          //Bitzustand Speichern
-     S88sendon = 's';  //Änderung vorgenommen. (SET)
-   }
-   S88RMCount++;
-  }
-  
-  // //--------------------------------------------------------------------------------------------
-  void notifyS88Data() {
-   if (S88sendon == 'i' || S88sendon == 'm') {
-     byte MAdr = 1;  //Rückmeldemodul
-     byte datasend[11];  //Array Gruppenindex (1 Byte) & Rückmelder-Status (10 Byte)
-     datasend[0] = 0; //Gruppenindex für Adressen 1 bis 10
-     for(byte m = 0; m < S88Module; m++) {  //Durchlaufe alle aktiven Module im Speicher
-       datasend[MAdr] = data[m];
-       MAdr++;  //Nächste Modul in der Gruppe
-       if (MAdr >= 11) {  //10 Module à 8 Ports eingelesen
-         MAdr = 1;  //beginne von vorn
-        //  EthSend (0x0F, 0x80, datasend, false, 0x02); //RMBUS_DATACHANED
-         z21.setS88Data(datasend);
-         datasend[0]++; //Gruppenindex erhöhen
-       }
-     }
-     if (MAdr < 11) {  //noch unbenutzte Module in der Gruppe vorhanden? Diese 0x00 setzten und dann Melden!
-       while (MAdr < 11) {
-         datasend[MAdr] = 0x00;  //letzten leeren Befüllen
-         MAdr++;   //Nächste Modul in der Gruppe   
-       }
+  S88RMCount++;
+}
+
+// //--------------------------------------------------------------------------------------------
+void notifyS88Data()
+{
+  if (S88sendon == 'i' || S88sendon == 'm')
+  {
+    byte MAdr = 1;     //Rückmeldemodul
+    byte datasend[11]; //Array Gruppenindex (1 Byte) & Rückmelder-Status (10 Byte)
+    datasend[0] = 0;   //Gruppenindex für Adressen 1 bis 10
+    for (byte m = 0; m < S88Module; m++)
+    { //Durchlaufe alle aktiven Module im Speicher
+      datasend[MAdr] = data[m];
+      MAdr++; //Nächste Modul in der Gruppe
+      if (MAdr >= 11)
+      {           //10 Module à 8 Ports eingelesen
+        MAdr = 1; //beginne von vorn
+                  //  EthSend (0x0F, 0x80, datasend, false, 0x02); //RMBUS_DATACHANED
+        z21.setS88Data(datasend);
+        datasend[0]++; //Gruppenindex erhöhen
+      }
+    }
+    if (MAdr < 11)
+    { //noch unbenutzte Module in der Gruppe vorhanden? Diese 0x00 setzten und dann Melden!
+      while (MAdr < 11)
+      {
+        datasend[MAdr] = 0x00; //letzten leeren Befüllen
+        MAdr++;                //Nächste Modul in der Gruppe
+      }
       //  EthSend (0x0F, 0x80, datasend, false, 0x02); //RMBUS_DATACHANED
       z21.setS88Data(datasend);
-     }
-     S88sendon = '0';        //Speicher Rücksetzten
-   }
+    }
+    S88sendon = '0'; //Speicher Rücksetzten
   }
+}
